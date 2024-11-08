743329a48fa32d63de86141b33bf3646e04cf5f1
daniil-gavrilyuk <bowie.D.G.D@gmail.com>
2024-11-09 01:30:32 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_1.py
@@ -0,0 +1,15 @@
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+
+# TODO заменить значение пропущенного элемента средним арифметическим
+
+
+
+missing_index = numbers.index(None)
+
+numbers_without_missing = [num for num in numbers if num is not None]
+
+average = round(sum(numbers_without_missing) / (len(numbers_without_missing)+1), 2)
+
+numbers[missing_index] = average
+
+print("Измененный список:", numbers)
